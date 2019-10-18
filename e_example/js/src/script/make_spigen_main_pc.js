b3c3094371c0b6504e9d992fdaf94639cef10401
JohnRo <heyho9012@gmail.com>
2019-10-18 17:48:01 +0900

    20191018

--- e_example/js/src/script/make_spigen_main_pc.js
+++ e_example/js/src/script/make_spigen_main_pc.js
@@ -15,6 +15,12 @@
   const openGnbBtn = $('.gnb_btn > button');
   const closeGnbBtn = $('.close_gnb_btn > button');
   const sideGnb = $ ('.side_gnb');
+  const gnbDl = gnb.find('dl');
+  const gnbDd = gnb.find('dd');
+  const gnbDt = gnb.find('dt');
+  const gnbTitleLink = gnbDt.children('a');
+  const gnbListLink = gnbDd.children('a');
+  
   let time = 600;
 
   openGnbBtn.on('click',function(e){
@@ -28,22 +34,19 @@
   });
 
   // #gnb에 마우스 올렸을 경우 dd를 나오게 만들기
-  const gnbDd = gnb.find('dd');
 
-  gnb.on('mouseenter',function(){
+  gnbTitleLink.on('mouseenter',function(){
+    $(this).addClass('action');
     gnbDd.stop().slideDown();
   });
 
   gnb.on('mouseleave',function(){
+    gnbTitleLink.removeClass('action');
     gnbDd.stop().slideUp();
   });
 
   // #gnb dt에 focus 처리되면 dd가 나타나게 만들기
   // 포커스가 끝난후 블러처리
-  const gnbDt = gnb.find('dt');
-  // a , button , form(input,textarea,select)
-  const gnbTitleLink = gnbDt.children('a');
-  const gnbListLink = gnbDd.children('a');
 
   gnbTitleLink.on('focus',function(){
     gnbDd.stop().slideDown();
