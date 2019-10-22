e9f5f77f4c025067adcc50787a23d2063514438a
goasb18 <goasb18@gmail.com>
2019-10-22 18:03:54 +0900

    20191022

---
+++ e_example/js/src/spigen_main.js
@@ -0,0 +1,49 @@
+// spigen_main.js
+
+(function($){
+  // const wrap    = $ ('#wrap'); // 1번째 방법
+  const body    = $ ('body'); // 2번째 방법
+  const header  = $ ('#headBox');
+  const adver   = $ ('#viewBox');
+  const content = $ ('#conBox');
+  const footer  = $ ('#footBox');
+//  ============================================
+ let tempUrl = "./spigen_temp/";
+ let jsUrl ="../js/src/spigen_temp_pc/";
+
+
+ /* 1번쨰 방법 ================================================================================== */
+ header.load(tempUrl + 'spigen_header.html',function(){
+  // $(this).after('<script src="../js/src/spigen_temp_pc/make_spigen_header_pc.js"></script>');
+ });
+ 
+ adver.load(tempUrl + 'spigen_main_adver.html');
+//  content =============================================================================================
+ content.load(tempUrl + 'spigen_main_content_01.html',function(){
+  
+  let conJS = jsUrl + 'make_spigen_content_01.js'; 
+  
+  // body.append(`<script src="../JS/src/spigen_temp_pc/make_spigen_content_01.js"></script>`); // 1번째 방법
+  // body.append('<script src="` + conJS + `"></script>'); // 2번째 방법
+  body.append(`<script src="${conJS}"></script>`); // 3번째 방법
+ });
+//  content ==============================================================================================
+ footer.load(tempUrl + 'spigen_footer.html');
+
+// setTimeout(function(){},'시간'); // 일정시간이 지난후에 함수를 수행해라!
+
+ setTimeout(function(){
+   body.append('<script src="../js/src/spigen_temp_pc/make_spigen_header_pc.js"></script>')
+ } ,100);
+
+
+ /* 1번쨰 방법 ================================================================================== */
+
+
+ /* 2번쨰 방법 ================================================================================== */
+// header.load(tempUrl + 'spigen_header.html',function(){
+//   body.append('<script src="../js/src/spigen_temp_pc/make_spigen_header_pc.js"></script>');
+// });
+ /* 2번쨰 방법 ================================================================================== */
+
+})(jQuery);
---
+++ e_example/js/src/spigen_temp_pc/make_spigen_content_01.js
@@ -0,0 +1,66 @@
+// make_spigen_content_01.js
+
+(function($){
+  // console.log('load');
+  const conArea = $('.content_area');
+  const conDt = conArea.find('dt');
+  // const conDtBtn = conDt.children('button');
+  const conDd = conArea.find('dd');
+  
+  conDt.children('button').on('click focus',function(e){
+    e.preventDefault();
+    let btn = $(this);
+    let btnParent = btn.parent();
+    let parNext = btnParent.next('dd');
+
+  btn.addClass('action');
+  btnParent.siblings('dt').children('button').removeClass('action');
+
+/* // step_1  =클릭시 컬러변환=
+  parNext.addClass('action');
+  parNext.siblings('dd').removeClass('action'); 
+*/
+
+   // step_2
+   parNext.stop().fadeIn();
+   parNext.siblings('dd').fadeOut();
+  //  action 클래스 이름으로 구분만 할 수 있도록 처리하고,
+  //  css에서 동작하지 않도록 처리
+  });
+
+  // ==================================================================================
+    const bmText = '\
+  <div class="album">\
+    <a href="">\
+      <h4></h4>\
+      <p class="con"></p>\
+      <p class="date"></p>\
+    </a>\
+  </div>';
+
+    const mediaList = [
+      {text:'[youtube] 아마존으로 2600억 매출',date:'2019.10.22'},
+      {text:'슈피겐코리아 미국 아마존 독점유통',date:'2019.10.20'},
+      {text:'[youtube] \'취업비자\' 기업탐방',date:'2019.10.10'}
+      ];
+    // mediaList[0].text 를 사용하면 가져올 수 있다.
+    // mediaList[1].date  
+    
+      // const blogList = ;
+
+    const media = conArea.find('.media');
+    // for(최초의값; 조건비교; 값의증감){}
+    for(let i = 0; i < mediaList.length; i++){
+      media.append(bmText);
+      let myNth = media.children('.album').eq(i);
+      
+      myNth.find('h4').text('media');
+      myNth.find('.con').text(mediaList[i].text);
+      myNth.find('.date').text(mediaList[i].date);
+    }
+
+
+
+  // ==================================================================================
+
+})(jQuery);
