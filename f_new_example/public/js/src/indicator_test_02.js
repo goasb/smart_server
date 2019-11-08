14249246ebc47c510ee681e353fc77a452d1d3e5
goasb18 <goasb18@gmail.com>
2019-11-08 17:57:31 +0900

    20191108

---
+++ f_new_example/public/js/src/indicator_test_02.js
@@ -0,0 +1,11 @@
+// indicator_test_02.js
+
+(function($){
+const miniProduct = $('#miniproduct'),
+btn = miniProduct.children('.btn'),
+
+indiLi = indicator.children('li'),
+indiLink = indicator.children('a'),
+
+
+})(jQuery);
